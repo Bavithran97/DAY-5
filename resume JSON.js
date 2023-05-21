c91077dc3467e19e405312e549a265ff5b5e69f6
Bavithran97 <bavithran9214@gmail.com>
2023-05-21 16:26:00 +0530

    first commit

---
+++ resume JSON.js
@@ -0,0 +1,35 @@
+let RESUME = 
+{
+    "name"            : "Bavithran",
+    "email"           : "bavithran9214@gmail.com",
+    "phone"           : 9360289247,
+    "degree"          : "Aeronautical Engineering",
+    "location"        : 
+    {
+        "address"     : "3/27, paum pon nagar , eriyodu",
+        "taluk"       : "Vedasandur",
+        "city"        : "Dindigul",
+        "state"       : "Tamilnadu",
+        "pincode"     :  624702
+    },
+    "Education"       : 
+    [
+    {
+    "institution"     : "RVS School of Engineering and Technology",
+    "courses"         : "Aeronautical Engineering",
+    "startDate"       : "2015-04-15",
+    "endDate"         : "2019-04-15",
+    "cgpa"            : 6.87
+    }
+    ],
+    "languages"       : "Tamil and English",
+    "skills"          : 
+    [
+    {
+    "name"            : "Full Stack Develper",
+    "level"           : "beginer",
+    }
+    ]
+}
+
+console.log(RESUME)
